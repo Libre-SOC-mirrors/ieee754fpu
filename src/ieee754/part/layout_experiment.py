556b84c70a49077a3532b8ae724472026b53d8ac
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-10-24 19:26:47 +0100

    spelling corrections after typing on a phone

--- src/ieee754/part/layout_experiment.py
+++ src/ieee754/part/layout_experiment.py
@@ -41,8 +41,8 @@ from ieee754.part_mul_add.partpoints import PartitionPoints
 # http://lists.libre-soc.org/pipermail/libre-soc-dev/2021-October/003921.html
 # XXX tempted to suggest that this function remain as a function, because
 # it takes all the context it needs as parameters.  its usefulness goes
-# beyond a dingle class, and there is actually nothing realistically
-# that it needs whixh is context-srnsitive.  theregore, on balabce,
+# beyond a single class, and there is actually nothing realistically
+# that it needs whixh is context-sensitive.  therefore, on balance,
 # it should remain a function
 def layout(elwid,            # comes from SimdScope constructor
            vec_el_counts,    # comes from SimdScope constructor
